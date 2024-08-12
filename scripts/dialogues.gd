6b483230c0f99541a17988fbff35f2fd6cdf4b9f
Sandra Moen <sandramoen01@gmail.com>
2024-08-12 15:22:44 +0200

    dialogues almost complete

--- scripts/dialogues.gd
+++ scripts/dialogues.gd
@@ -2,144 +2,129 @@ class_name Dialogues
 extends Node
 
 var dialogue_items: Array[Dictionary] = [
-	{
-		"expressionA": "very angry",
+	{ # 0
+		"expressionA": "rage",
 		"expressionB": "serious",
-		"text": "0Hey, [b]What[/b] are you doing?",
+		"text": "You're sick in the head, what's wrong with [shake]you[/shake]?!",
 		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
+			"I understand you're upset.
+				I'm scared, may you please
+				leave me alone?": 0,
+			"I feel like you shouldn't
+				talk like that, to anyone!": -1,
+			"Whatever, weirdo": -2,
+		},
+	},
+	{ # 1
+		"expressionA": "fear",
+		"expressionB": "serious",
+		"text": "You'll never be a woman you freak. Your just a [shake]biological[/shake] man in a dress!",
+		"choices": {
+			"You’re worried about safety,
+				And questioning my identity?
+				Is that right?": 0,
+			"Why are you being so mean?": -1,
+			"What the fuck is wrong with you?": -2,
 			
 		},
 	},
-	{
+	{ # 2
 		"expressionA": "very angry",
-		"expressionB": "serious",
-		"text": "1Hey, [b]What[/b] are you doing?",
+		"expressionB": "tired",
+		"text": "Women and girls need to be safe in the bathroom from [shake]freaks[/shake] like you!",
 		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
+			"So, you’re concerned about
+				the safety of women and girls?": 0,
+			"Leave me alone, weirdo": -1,
+			"You're insufferable!": -2,			
+		},
+	},
+	{ # 3
+		"expressionA": "rage",
+		"expressionB": "eyes closed",
+		"text": "You're [shake]not[/shake] a woman! You can't go in there!",
+		"choices": {
+			"I’m feeling hurt.
+				I need to be seen as I am.
+				May you please leave me alone?": 0,
+			"Oh, are we doing one of these?": -1,
+			"What's wrong with you?": -2,
 			
 		},
 	},
-	{
-		"expressionA": "very angry",
-		"expressionB": "serious",
-		"text": "2Hey, [b]What[/b] are you doing?",
+	{ # 4
+		"expressionA": "driven",
+		"expressionB": "annoyed",
+		"text": "That's the [shake]women's[/shake] bathroom!",
 		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
+			"I'm feeling scared right now.
+				I just needed to use the bathroom": 0,
+			"Yes, and I'm a woman?": -1,
+			"Fuck off transphobe": -2,
 			
 		},
 	},
-	{
+	{ # 5
 		"expressionA": "very angry",
 		"expressionB": "serious",
-		"text": "3Hey, [b]What[/b] are you doing?",
+		"text": "Hey, What are you doing?",
 		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
+			"I'm feeling a bit confused.
+				Would you please
+				tell me what's up?": 0,
+			"I... went to the bathroom?": -1,
+			"Get lost, weirdo!": -2,
 			
 		},
 	},
-	{
-		"expressionA": "very angry",
-		"expressionB": "serious",
-		"text": "4Hey, [b]What[/b] are you doing?",
+	{ # 6
+		"expressionA": "explaining",
+		"expressionB": "smile",
+		"text": "Do you think that's okay? Transgender men like you shouldn't use the women's bathroom!",
 		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
+			"So, you're worried about men
+				using the women's bathroom?": 0,
+			"The women's bathroom
+				are for all women": -1,
 			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
-			
+				Clearly, you're an idiot...": -2,
 		},
 	},
-	{
+	{ # 7
 		"expressionA": "very angry",
 		"expressionB": "serious",
-		"text": "5Hey, [b]What[/b] are you doing?",
+		"text": "You need to be respectful of women who are traumatized by penises!",
 		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
-			
+			"So, you're wanting respect
+				for women, especially those
+				who are traumatized?": 0,
+			"You need to be respectful,
+				in general": -1,
+			"The fuck are you on about?": -2,
 		},
 	},
-	{
+	{ # 8
 		"expressionA": "very angry",
-		"expressionB": "serious",
-		"text": "6Hey, [b]What[/b] are you doing?",
+		"expressionB": "smile",
+		"text": "I just want everyone to be safe. And I feel you should want that too.",
 		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
-			
+			"I also want everyone to be
+				safe. And right now I'm
+				feeling kinda scared": 0,
+			"How about leaving
+				me alone then?": -1,
+			"Cool story, bro": -2,
 		},
 	},
-	{
+	{ # 9
 		"expressionA": "very angry",
-		"expressionB": "serious",
-		"text": "7Hey, [b]What[/b] are you doing?",
+		"expressionB": "contempt",
+		"text": "Well, I still don't feel you should use the women's bathroom. It's very rude of you.",
 		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
-			
-		},
-	},
-	{
-		"expressionA": "very angry",
-		"expressionB": "serious",
-		"text": "8Hey, [b]What[/b] are you doing?",
-		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
-			
-		},
-	},
-	{
-		"expressionA": "very angry",
-		"expressionB": "serious",
-		"text": "9Hey, [b]What[/b] are you doing?",
-		"choices": {
-			"I'm sorry?
-				Would like to tell me
-			 	what's the matter?": 0,
-			"What?
-				Leave me alone": -1,
-			"Shut up!": -2,
-			
+			"I hear you think it's rude.
+				I need to go now. Goodbye": 0,
+			"Well, no one asked you...": -1,
+			"Get lost...": -2,
 		},
 	},
 ]
--- scripts/game.gd
+++ scripts/game.gd
@@ -3,6 +3,17 @@ extends Control
 var faces := {
 	"very angry": preload ("res://assets/faces/Very Angry.svg"),
 	"serious": preload ("res://assets/faces/Serious.svg"),
+	"driven": preload ("res://assets/faces/Driven.svg"),
+	"eyes closed": preload ("res://assets/faces/Eyes Closed.svg"),
+	"rage": preload ("res://assets/faces/Rage.png"),
+	"annoyed": preload ("res://assets/faces/Annoyed.png"),
+	"fear": preload ("res://assets/faces/Fear.svg"),
+	"solemn": preload ("res://assets/faces/Solemn.svg"),
+	"tired": preload ("res://assets/faces/Tired.svg"),
+	"hectic": preload ("res://assets/faces/Hectic.svg"),
+	"explaining": preload ("res://assets/faces/Explaining.png"),
+	"smile": preload ("res://assets/faces/Smile.svg"),
+	"contempt": preload ("res://assets/faces/Contempt.svg"),
 }
 
 var Dialogues = preload("res://scripts/dialogues.gd")  # Preload the script
@@ -31,7 +42,7 @@ func _ready():
 	animation_player.play("intro")
 	
 	toilet_sounds.play()
-	
+		
 	ambiant_sounds.play(randf() * ambiant_sounds.stream.get_length())
 	var tween := create_tween()
 	tween.tween_interval(2.0)
@@ -55,8 +66,51 @@ func create_buttons(choices_data: Dictionary):
 		var button := Button.new()
 		button.text = choice_text
 		var target_line_idx: int = choices_data[choice_text]
-		button.pressed.connect(show_text.bind(target_line_idx))		
+		button.pressed.connect(show_text.bind(target_line_idx))
+
+		# Create a new Theme for each button
+		var theme := Theme.new()
+
+		# Create a StyleBoxFlat for the normal state
+		var normal_stylebox := StyleBoxFlat.new()
+		normal_stylebox.bg_color = Color(0.0, 0.0, 0.0)  # Set the default color if needed
+		normal_stylebox.corner_radius_bottom_left = 8
+		normal_stylebox.corner_radius_bottom_right = 8
+		normal_stylebox.corner_radius_top_right = 8
+		normal_stylebox.corner_radius_top_left = 8
+		normal_stylebox.content_margin_left = 11
+		normal_stylebox.content_margin_top = 8
+		normal_stylebox.content_margin_right = 11
+		normal_stylebox.content_margin_bottom = 9
+		
+		# Create a StyleBoxFlat for hover state
+		var hover_stylebox := StyleBoxFlat.new()
+
+		# Set hover color based on target_line_idx
+		if target_line_idx == -2:
+			hover_stylebox.bg_color = Color("#670038") # red
+		elif target_line_idx == -1:
+			hover_stylebox.bg_color = Color("5c4100") # orange
+		elif target_line_idx == 0:
+			hover_stylebox.bg_color = Color("004136") # green
+		
+		# Set the corner radius for the hover state
+		hover_stylebox.corner_radius_bottom_left = 8
+		hover_stylebox.corner_radius_bottom_right = 8
+		hover_stylebox.corner_radius_top_right = 8
+		hover_stylebox.corner_radius_top_left = 8
+		
+		# Assign the normal and hover styles to the button's theme
+		theme.set_stylebox("normal", "Button", normal_stylebox)
+		theme.set_stylebox("hover", "Button", hover_stylebox)
+		theme.set_stylebox("pressed", "Button", hover_stylebox)
+
+		# Apply the theme to the button
+		button.theme = theme
+
+		# Add the button to the buttons array
 		buttons.append(button)
+
 		
 	_add_buttons_in_random_order(buttons)
 
@@ -70,6 +124,7 @@ func show_text(item_index: int, is_animate_body_b: bool = true):
 	var current_item: Dictionary = dialogue_items[levels[current_level]]
 	levels.remove_at(current_level)
 	
+	#rich_text_label.text = "[center]" + current_item["text"] + "[/center]"
 	rich_text_label.text = current_item["text"]
 	expression_a.texture = faces[current_item["expressionA"]]
 	expression_b.texture = faces[current_item["expressionB"]]
@@ -77,7 +132,7 @@ func show_text(item_index: int, is_animate_body_b: bool = true):
 		
 	# animations
 	var tween := create_tween()
-	var body_a_interval_duration = randf_range(1.2, 1.5)
+	var body_a_interval_duration = randf_range(1.0, 1.3)
 	animate_text(tween, current_item["text"], body_a_interval_duration)
 	animate_buttons(tween)
 	animate_body_A(body_a_interval_duration)
@@ -86,7 +141,7 @@ func show_text(item_index: int, is_animate_body_b: bool = true):
 	
 	
 func animate_text(tween, text: String, interval_duration: float):
-	var text_speed := 25.0
+	var text_speed := 21.0
 	var text_appearing_duration: float = text.length() / text_speed
 	rich_text_label.modulate.a = 0.0
 	rich_text_label.visible_ratio = 0.0
@@ -102,9 +157,10 @@ func animate_buttons(tween: Tween):
 		
 	tween.finished.connect(func():
 		var button_tween = create_tween()
+		button_tween.tween_interval(0.5)
 		for button: Button in buttons_v_box_container.get_children():
 			button.disabled = false
-			var duration := clampf(button.text.length() / 20.0, 0.3, 1.0)
+			var duration := clampf(button.text.length() / 50.0, 0.3, 0.7)
 			button_tween.tween_property(button, "modulate:a", 1.0, duration)
 	)
 
